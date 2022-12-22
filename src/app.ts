b27727e13f5b962a56a405001f75a5a8f16e4b8f
Leandro Assis <leandrou9@gmail.com>
2022-12-22 20:06:26 -0300

    Type Guards (Compass.Uol)

--- src/app.ts
+++ src/app.ts
@@ -20,3 +20,59 @@ const e1: EmpregadoElevado = {
 type Combinavel = string | number;
 type Numerico = number | boolean;
 type Universal = Combinavel & Numerico;
+
+function soma(a: Combinavel, b: Combinavel) {
+    //type guard 
+    //tipo Combinavel pode ser string ou number (condicional para cada tipo)
+    if (typeof a === 'string' || typeof b === 'string') {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+
+type EmpregadoDesconhecido = Empregado | Admin; //union type
+
+function mostraInfoEmpregado(emp: EmpregadoDesconhecido) {
+    console.log('Nome: ' + emp.nome);
+    if ('privilegios' in emp) {
+        console.log('Privilegios: ' + emp.privilegios);
+    }
+    if ('dataInicio' in emp) {
+        console.log('Data de Início: ' + emp.dataInicio);
+    }
+}
+
+mostraInfoEmpregado(e1);
+mostraInfoEmpregado({nome: 'Lisandro', dataInicio: new Date()});
+
+class Carro {
+    drive() {
+        console.log('FAR... awaaaay');
+    }
+}
+
+class Caminhao {
+    drive() {
+        console.log('Uma paixão chamada Scania');
+    }
+
+    cargaPesada(quantidade: number) {
+        console.log('Eu conheço todas as cidades...' + quantidade);
+    }
+}
+
+type Veiculo = Carro | Caminhao;
+
+const v1 = new Carro();
+const v2 = new Caminhao();
+
+function usaVeiculo(veiculo: Veiculo) {
+    veiculo.drive();
+    //type guard mais elegante!!!
+    //instanceof só serve para classes, não para interfaces
+    if (veiculo instanceof Caminhao) {
+        veiculo.cargaPesada(1000);
+    }
+}
+usaVeiculo(v1);
+usaVeiculo(v2);
