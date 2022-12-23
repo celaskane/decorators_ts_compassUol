3e6a6121992715141da93713e4e27b647923520f
Leandro Assis <leandrou9@gmail.com>
2022-12-23 09:17:23 -0300

    Generic Functions e Constraints (Compass.Uol)

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+const nomes: Array<string | number> = [];   //generic type
+//nomes.[0].split(' ');
+
+// Promisses
+const promise: Promise<any> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Está feito!');     //com <any> é possível passar qualquer tipo de dado aqui
+    }, 2000);
+});
+
+promise.then(data => {
+    data.split(' ');
+});
+
+// Criando uma Generic Function (não funciona sem o extends)
+/* function merge<T, U>(objA: T, objB: U) {
+    return Object.assign(objA, objB);
+}
+console.log(merge({nome: 'Alejandro'}, {idade: 77}));
+
+const mergedOBj = merge({nome: 'Alejandro'}, {idade: 77}); */
+
+// Type Constraints (extends)
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+    return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({nome: 'Alejandro', hobbies: ['Qualquer']}, {idade: 77});
+console.log(mergedObj);
