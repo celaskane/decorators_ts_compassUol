043272400250de0163421109c51a960f3778597b
Leandro Assis <leandrou9@gmail.com>
2022-12-23 11:48:23 -0300

    Construindo Decorators mais úteis (Compass.Uol)

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,20 @@ function Entrar(textoEntrada: string) {
     };
 }
 
-@Entrar('Entrando...')
+function ComTemplate(template: string, puxaId: string) {
+    // _ para simbolizar queo constructor não será utilizado
+    return function(constructor: any) {
+        const puxaElemento = document.getElementById(puxaId);
+        const p = new constructor();
+        if (puxaElemento) {
+            puxaElemento.innerHTML = template;
+            puxaElemento.querySelector('h1')!.textContent = p.nome;
+        }
+    }
+}
+
+//@Entrar('Entrando...')
+@ComTemplate('<h1>Meu Objeto Pessoal</h1>', 'app')
 class Pessoa {
     nome = 'Alejandro';
 
